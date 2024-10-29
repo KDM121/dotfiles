8dfec2d954fc2c106ab8010dd1059219f42a9138
KDM121 <117851257+KDM121@users.noreply.github.com>
2024-10-29 13:31:44 +0000

    Update masterscript.sh

--- masterscript.sh
+++ masterscript.sh
@@ -1,156 +1,101 @@
 #!/bin/bash
 
+# Function to get user input for username and password
+get_user_input() {
+  username=$(whiptail --inputbox "Enter username" 10 30 --title "Username" 3>&1 1>&2 2>&3)
+  password=$(whiptail --passwordbox "Enter password" 10 30 --title "Password" 3>&1 1>&2 2>&3)
+}
 
-while true; do
-read -p "Do you want to update? (y/n) " yn
+# Main loop
+choices=$(whiptail --checklist "Select options" 25 50 8 \
+  "1" "Update and Upgrade" on \
+  "2" "Enable Automatic Update and Upgrade" on \
+  "3" "Install curl and sudo" on \
+  "4" "Create a new user" off \
+  "5" "Create a new sudo user" off \
+  "6" "Install podman" off \
+  "7" "Set dotfiles" on \
+  "8" "Disable root" off 3>&1 1>&2 2>&3)
 
-case $yn in
-        [yY] )  echo "Update and Upgrading"
-                # Update and Upgrade
-                apt update
-                apt upgrade -y
-                apt dist-upgrade -y
-                apt autoremove -y
-                apt autopurge -y
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
+if [ $? -eq 0 ]; then
+  echo "You selected: $choices"
+else
+  echo "No options selected."
+  exit 1
+fi
 
-
-while true; do
-read -p "Do you want to enable Automatic Update and Upgrade? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "Automatic Update and Upgrade"
-                # Automatic Update and Upgrade
-                echo "enabling auto updates"
-                apt install unattended-upgrades -y
-                dpkg-reconfigure --priority=low unattended-upgrades
-                distro_codename=$(lsb_release -c -s)
-                tee -a /etc/apt/apt.conf.d/50unattended-upgrades << EOF
-        "origin=Debian,codename=${distro_codename}-security";
-        "origin=Debian,codename=${distro_codename}-updates";
+for choice in $(echo $choices | tr -d '\"'); do
+    case $choice in
+      1)
+        echo "Update and Upgrading"
+        apt update
+        apt upgrade -y
+        apt dist-upgrade -y
+        apt autoremove -y
+        apt autopurge -y
+        ;;
+      2)
+        echo "Automatic Update and Upgrade"
+        apt install unattended-upgrades -y
+        dpkg-reconfigure --priority=low unattended-upgrades
+        distro_codename=$(lsb_release -c -s)
+        tee -a /etc/apt/apt.conf.d/50unattended-upgrades << EOF
+"origin=Debian,codename=${distro_codename}-security";
+"origin=Debian,codename=${distro_codename}-updates";
 EOF
-                sudo systemctl enable --now unattended-upgrades
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
+        systemctl enable --now unattended-upgrades
+        ;;
+      3)
+        echo "Install prereqs"
+        apt install curl -y
+        apt install sudo -y
+        ;;
+      4)
+        echo "Create new non-admin user"
+        get_user_input
+        username1=$username
+        password1=$password
+        adduser "$username1"
+        echo "$username1:$password1" | chpasswd
+        ;;
+      5)
+        echo "Create sudo user"
+        get_user_input
+        username2=$username
+        password2=$password
+        adduser "$username2"
+        echo "$username2:$password2" | chpasswd
+        usermod -aG sudo "$username2"
+        ;;
+      6)
+        echo "Install podman"
+        apt-get -y install podman
+        systemctl --user enable --now podman.socket
+        apt install podman-compose -y
+        ;;
+      7)
+        echo "Set dotfiles"
+        # Download
+        wget -O /home/"$username1"/.bashrc https://raw.githubusercontent.com/KDM121/dotfiles/refs/heads/main/server-bashrc
+        wget -O /home/"$username2"/.bashrc https://raw.githubusercontent.com/KDM121/dotfiles/refs/heads/main/server-bashrc
+        wget -O /etc/ssh/ssh_config.d https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-ssh-config-d
+        wget -O /home/"$username1"/.inputrc https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-inputrc
+        wget -O /home/"$username2"/.inputrc https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-inputrc
 
+        # Set permissions
+        chown "$username1":"$username1" /home/"$username1"/.bashrc /home/"$username1"/.inputrc
+        chown "$username2":"$username2" /home/"$username2"/.bashrc /home/"$username2"/.inputrc
+        chmod 644 /home/"$username1"/.bashrc /home/"$username1"/.inputrc
+        chmod 644 /home/"$username2"/.bashrc /home/"$username2"/.inputrc
+        chmod 755 /etc/ssh/ssh_config.d/default.conf
 
-while true; do
-read -p "Do you want to install curl and sudo? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "Install prereqs"
-                # Install prereqs
-                apt install curl -y
-                apt install sudo -y
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
-
-
-while true; do
-read -p "Do you want to create a new user? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "create new non-admin user"
-                # create new non-admin user
-                read -p "set username: " username1
-                echo "creating user $username1";
-                adduser "$username1";
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
-
-
-while true; do
-read -p "Do you want to create a new sudo user? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "create sudo user"
-                read -p "set sudo username: " username2
-                echo "creating user $username2";
-                adduser "$username2";
-
-                echo "add user to sudo group";
-                usermod -aG sudo "$username2"
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
-
-
-while true; do
-read -p "Do you want to install podman? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "install podman"
-                # install podman
-                apt-get -y install podman
-                systemctl --user enable --now podman.socket
-                apt install podman-compose -y
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
-
-
-while true; do
-read -p "Do you want to set dotfiles? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "set dofiles"
-                # set dotfiles
-                wget -O /home/"$username1"/.bashrc https://raw.githubusercontent.com/KDM121/dotfiles/refs/heads/main/server-bashrc
-                wget -O /home/"$username2"/.bashrc https://raw.githubusercontent.com/KDM121/dotfiles/refs/heads/main/server-bashrc
-                wget -O /etc/ssh/ssh_config.d https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-ssh-config-d
-                wget -O /home/"$username1"/.inputrc https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-inputrc
-                wget -O /home/"$username2"/.inputrc https://github.com/KDM121/dotfiles/raw/refs/heads/main/server-inputrc
-                # Set correct ownership and permissions
-                chown "$username1":"$username1" /home/"$username1"/.bashrc /home/"$username1"/.inputrc
-                chown "$username2":"$username2" /home/"$username2"/.bashrc /home/"$username2"/.inputrc
-                chmod 644 /home/"$username1"/.bashrc /home/"$username1"/.inputrc
-                chmod 644 /home/"$username2"/.bashrc /home/"$username2"/.inputrc
-                chmod 755 /etc/ssh/ssh_config.d/default.conf
-                
-                # Source the .bashrc files
-                su - "$username1" -c "source /home/$username1/.bashrc"
-                su - "$username2" -c "source /home/$username2/.bashrc"
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
-
-
-while true; do
-read -p "Do you want to disable root? (y/n) " yn
-
-case $yn in
-        [yY] )  echo "disabling root"
-                # disable root
-                passwd -l root
-                break;;
-        [nN] )  echo "skipping";
-                break;;
-        *)      echo "invalid response";;
-esac
-done
+        # Source bashrc
+        su - "$username1" -c "source /home/$username1/.bashrc"
+        su - "$username2" -c "source /home/$username2/.bashrc"
+        ;;
+      8)
+        echo "Disabling root"
+        passwd -l root
+        ;;
+    esac
+  done
