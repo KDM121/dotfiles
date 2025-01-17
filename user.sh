3c15415a665a4247b9e8250991dd9e4c135def20
KDM121 <117851257+KDM121@users.noreply.github.com>
2025-01-17 23:08:53 +0000

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,13 +2,13 @@
 
 # Prompt for the new user's name and password
 read -p "Enter the new user's name: " username
-read -s -p "Enter the new user's password: " password
-echo
+#read -s -p "Enter the new user's password: " password
+#echo
 
 # Create the new user with the provided name
-sudo useradd -m -s/bin/bash -c "$username" $username
-
+#sudo useradd -m -s/bin/bash -c "$username" $username
+adduser $username
 # Set the new user's password 
-echo "$username:$password" | sudo chpasswd
+#echo "$username:$password" | sudo chpasswd
 
 echo "New user '$username' has been created."
